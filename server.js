ae17c7be08f23bf575001a17d7b852f0f725b334
Esteban Reyes Marcelo <e.reyesm@uniandes.edu.co>
2024-10-07 14:45:45 -0500

    Configuracion para deploy en Heroku v2

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + "/dist/proyecto-frontend"));
+app.use(express.static(__dirname + "/dist/abcall-web"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/proyecto-frontend/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/abcall-web/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
