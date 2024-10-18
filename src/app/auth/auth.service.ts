5ff438606744e1205d82bef4a603fec25b69346c
Esteban Reyes Marcelo <e.reyesm@uniandes.edu.co>
2024-10-18 11:06:33 -0500

    Deploy ajuste proxy

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -8,16 +8,13 @@ import { Observable, map, throwError } from 'rxjs';
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = environment.baseUrl;
-
   constructor(private http: HttpClient) {}
 
   login(user: User): Observable<string> {
-    console.log(this.apiUrl);
     const headers = { 'Content-Type': 'application/json' };
     if (user.role === 'CLIENT') {
       return this.http
-        .post<string>(`${this.apiUrl}/auth/clients/token`, user, { headers })
+        .post<string>('/auth/clients/token', user, { headers })
         .pipe(
           map((response: any) => {
             const token = response.token;
@@ -26,7 +23,7 @@ export class AuthService {
         );
     } else if (user.role === 'AGENT') {
       return this.http
-        .post<string>(`${this.apiUrl}/auth/agents/token`, user, { headers })
+        .post<string>('/auth/agents/token', user, { headers })
         .pipe(
           map((response: any) => {
             const token = response.token;
