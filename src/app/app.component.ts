9a97636ab403a23cc6fc4bfbb04e07fe131a80d0
Esteban Reyes Marcelo <e.reyesm@uniandes.edu.co>
2024-10-24 15:07:40 -0500

    Se agrega el menu y el log out dinamico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'abcall-web';
+  shouldShowMenu = false;
+  shouldShowLogOut = false;
+  shouldShowBackOption = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // Listen to route changes
+    this.router.events.subscribe(() => {
+      this.shouldShowMenu =
+        this.router.url.includes('auth') == false &&
+        this.router.url.includes('consumer');
+
+      this.shouldShowLogOut =
+        this.router.url.includes('auth') == false &&
+        this.router.url.includes('consumer');
+
+      this.shouldShowBackOption = this.router.url.includes('auth');
+    });
+  }
+
+  goToConsumer() {
+    this.router.navigate(['/consumer']);
+  }
+
+  logOut() {
+    sessionStorage.removeItem('user');
+    this.router.navigate(['/auth']);
+  }
 }
