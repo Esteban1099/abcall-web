d44c3018698b72df783c29bc24171cafe033e141
Fernando Garzon <of.garzon2662@uniandes.edu.co>
2024-11-08 18:54:50 -0800

    DEPLOY

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,14 +2,14 @@ import {defineConfig} from 'cypress'
 
 export default defineConfig({
 
-  //e2e: {
-    //'baseUrl': 'https://abcall-web-460072ffa983.herokuapp.com/es-CO/auth',
-  //
-  //},
-
   e2e: {
-    baseUrl: 'http://localhost:4200/auth',
+    'baseUrl': 'https://abcall-web-460072ffa983.herokuapp.com/es-CO/auth',
+
   },
+
+  // e2e: {
+  //   baseUrl: 'http://localhost:4200/auth',
+  // },
 
   component: {
     devServer: {
