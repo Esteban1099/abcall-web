dc0f847be184dbce6dea5b11c811babe55e4df5a
Fernando Garzón <of.garzon2662@uniandes.edu.co>
2024-11-08 19:19:05 -0800

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,12 +4,8 @@ export default defineConfig({
 
   e2e: {
     'baseUrl': 'https://abcall-web-460072ffa983.herokuapp.com/es-CO/auth',
-
   },
 
-  // e2e: {
-  //   baseUrl: 'http://localhost:4200/auth',
-  // },
 
   component: {
     devServer: {
