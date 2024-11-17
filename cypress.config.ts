13de5281db7f19e37d317eca704052b5a513e55d
Fernando Garzon <of.garzon2662@uniandes.edu.co>
2024-11-17 09:53:51 -0800

    e2e tests passing

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,22 +1,15 @@
-import {defineConfig} from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-
   e2e: {
-    'baseUrl': 'https://abcall-web-460072ffa983.herokuapp.com/es-CO/',
-
+    baseUrl: 'https://abcall-web-460072ffa983.herokuapp.com/es-CO/',
   },
-
-  // e2e: {
-  //   baseUrl: 'http://localhost:4200/auth',
-  // },
 
   component: {
     devServer: {
       framework: 'angular',
       bundler: 'webpack',
     },
-    specPattern: '**/*.cy.ts'
-  }
-
-})
+    specPattern: '**/*.cy.ts',
+  },
+});
